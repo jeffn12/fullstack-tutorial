593754287d0ee2735b7e3d38d20642481e92ac76
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-14 17:32:49 -0400

    feat: add function to get launches by arr of ids

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -18,6 +18,10 @@ class LaunchAPI extends RESTDataSource {
     return this.launchReducer(response[0])
   }
 
+  getLaunchesByIds({launchIds}) {
+    return Promise.all(launchIds.map(launchId => this.getLaunchById({launchId})))
+  }
+
   launchReducer(launch) {
     return {
       id: launch.flight_number || 0,
